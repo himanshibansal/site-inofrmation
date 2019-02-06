7bee7ea61c2fc2a62d65ba1b975bba0e0386ee2b
Himanshi Bansal <hkbansal@cygnet.com>
2019-02-06 16:44:15 +0530

    code deploy

---
+++ site_information/src/Controller/SiteInformationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\site_information\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
+
+class SiteInformationController extends ControllerBase{
+	function site_information_form_submit(&$form, FormStateInterface $form_state) {
+	    exit("tets");
+  	}
+}
+
